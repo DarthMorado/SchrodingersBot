155244c4a58b6a548232f61b2d323944b439bd05
DarthMorado <DarthMoradoDev@gmail.com>
2025-10-25 15:03:20 +0300

    start

---
+++ SchrodingersBot/SchrodingersBot/Commands/StartCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using NotABot.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchrodingersBot.Commands
+{
+    public class StartCommandHandler : IBotCommandHandler<startCommand>
+    {
+        public async Task<List<Answer>> Handle(startCommand request, CancellationToken cancellationToken)
+        {
+            return new()
+            {
+                new()
+                {
+                    ChatId = request.Message.ChatId,
+                    AnswerType = "message",
+                    Text =
+@"This is Bot
+v\.0\.1\.
+"
+                }
+            };
+        }
+    }
+}
