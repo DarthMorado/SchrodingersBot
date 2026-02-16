31c9acfe2195ec1900c85c7187dc8fe4b938d7a2
DarthMorado <DarthMoradoDev@gmail.com>
2026-02-17 01:53:41 +0200

    1.3.0.0. Stable

---
+++ SchrodingersBot/SchrodingersBot/Commands/Common/helpCommand.cs
@@ -0,0 +1,13 @@
+﻿using NotABot.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchrodingersBot.Commands
+{
+    public class helpCommand : BotCommand
+    {
+    }
+}
