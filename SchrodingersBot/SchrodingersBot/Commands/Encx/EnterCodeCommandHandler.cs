aab728e9f03b7fde02e503bbe74ae1ec54337fd7
DarthMorado <DarthMoradoDev@gmail.com>
2026-02-18 01:29:19 +0200

    v.1.3.1. tested

--- SchrodingersBot/SchrodingersBot/Commands/Encx/EnterCodeCommandHandler.cs
+++ SchrodingersBot/SchrodingersBot/Commands/Encx/EnterCodeCommandHandler.cs
@@ -17,12 +17,15 @@ namespace SchrodingersBot.Commands
         private readonly IGameService _gameService;
         private readonly IEncxEngine _encxEngine;
         private readonly IDbRepository<EncxGameSubscriptionEntity> _subscriptionsRepository;
+        private readonly IDbRepository<ChatParameterEntity> _chatParametersRepository;
 
         public EnterCodeCommandHandler(IGameService gameService,
             IEncxEngine encxEngine,
-            IDbRepository<EncxGameSubscriptionEntity> subscriptionsRepository
+            IDbRepository<EncxGameSubscriptionEntity> subscriptionsRepository,
+            IDbRepository<ChatParameterEntity> chatParametersRepository
             )
         {
+            _chatParametersRepository = chatParametersRepository;
             _subscriptionsRepository = subscriptionsRepository;
             _gameService = gameService;
             _encxEngine = encxEngine;
@@ -31,7 +34,7 @@ namespace SchrodingersBot.Commands
         public async Task<Result> Handle(entercodeCommand request, CancellationToken cancellationToken)
         {
             bool? isCorrect = null;
-
+            
             var game = await _gameService.GetActiveGame(request.Message.ChatId);
 
             if (game is null)
@@ -39,7 +42,23 @@ namespace SchrodingersBot.Commands
                 return null;
             }
 
+            var prefixData = await _chatParametersRepository.FindAsync(x => x.ChatId == request.Message.ChatId && x.Code == "PREFIX");
+            string prefix = null;
+            if (prefixData != null && prefixData.Any())
+            {
+                prefix = prefixData.First().TextValue;
+            }
+
             var gameState = await _encxEngine.EnterCode(game.LoginInfo, game.ActiveLevelId, game.ActiveLevelNumber, request.Message.Parameter);
+            if (!(gameState?.EngineAction?.LevelAction?.IsCorrectAnswer ?? false) &&
+                !(gameState?.EngineAction?.BonusAction?.IsCorrectAnswer ?? false) &&
+                !String.IsNullOrWhiteSpace(prefix) &&
+                !request.Message.Parameter.StartsWith(prefix)
+                )
+            {
+                var newCode = $"{prefix}{request.Message.Parameter}";
+                gameState = await _encxEngine.EnterCode(game.LoginInfo, game.ActiveLevelId, game.ActiveLevelNumber, newCode);
+            }
 
             Answer.ReactionType resultType = Answer.ReactionType.Unknown;
 
